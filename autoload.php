d863f991db9f7eea4c8d1885c8d7c777430a794b
Elusive <18050480+elusivecodes@users.noreply.github.com>
2024-03-26 19:23:20 +1000

    - Added ROOT const to autoload.

--- autoload.php
+++ autoload.php
@@ -7,12 +7,13 @@ use Fyre\Utility\Path;
 
 // Constants
 define('TIME_START', hrtime());
-define('APP', PATH::join(__DIR__, 'app'));
-define('CONFIG', PATH::join(__DIR__, 'config'));
-define('LANG', PATH::join(__DIR__, 'language'));
-define('LOG', PATH::join(__DIR__, 'log'));
-define('TEMPLATES', PATH::join(__DIR__, 'templates'));
-define('TMP', PATH::join(__DIR__, 'tmp'));
+define('ROOT', __DIR__);
+define('APP', PATH::join(ROOT, 'app'));
+define('CONFIG', PATH::join(ROOT, 'config'));
+define('LANG', PATH::join(ROOT, 'language'));
+define('LOG', PATH::join(ROOT, 'log'));
+define('TEMPLATES', PATH::join(ROOT, 'templates'));
+define('TMP', PATH::join(ROOT, 'tmp'));
 
 // Register autoloader
 Loader::addClassMap($composer->getClassMap());
